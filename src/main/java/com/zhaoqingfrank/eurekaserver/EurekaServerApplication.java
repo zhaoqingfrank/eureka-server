f6471e2782680758ec9c22c4b684bdd333c739e7
zhaoqingfrank <2267581731@qq.com>
2018-05-17 19:03:28 +0800

    https://github.com/zhaoqingfrank/eureka-server.git

---
+++ src/main/java/com/zhaoqingfrank/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.zhaoqingfrank.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+	}
+}
